cf35d19bcb642322249aac2efc6134e732e09b0d
shivamshete1036 <159644256+shivamshete1036@users.noreply.github.com>
2026-08-20 21:59:34 +0530

    github mcp server implemented

---
+++ app.py
@@ -0,0 +1,83 @@
+import asyncio
+import os
+from dotenv import load_dotenv
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langchain_ollama import ChatOllama
+from langchain.agents import create_agent
+load_dotenv()
+
+async def main():
+
+    client = MultiServerMCPClient({
+        "github": {
+            "url": "https://api.githubcopilot.com/mcp/",
+            "transport": "streamable_http",
+            "headers": {
+                "Authorization": f"Bearer {os.environ['GITHUB_TOKEN']}"
+            }
+        }
+    })
+
+    tools = await client.get_tools()
+
+    # print("\nAvailable MCP Tools:")
+    # for tool in tools:
+    #     print(f"- {tool.name}")
+
+    llm = ChatOllama(
+        model="gpt-oss:120b-cloud"
+    )
+
+    agent = create_agent(
+        llm,
+        tools
+    )
+
+    prompt = (
+    """
+    Using my authenticated GitHub account, the owner is shivamshete1036,
+    give me the details of the repository named as enterprise-knowledge-assistant in simple language,
+    and also help to summarize the content of app.py file in that repository 
+    
+            
+    """
+        
+    )
+
+    result = await agent.ainvoke({
+        "messages": [
+            ("user", prompt)
+        ]
+    })
+
+    print("\n" + "=" * 60)
+    print("TOOL CALLS")
+    print("=" * 60)
+
+    for message in result["messages"]:
+
+        # Check whether this message contains tool calls
+        if hasattr(message, "tool_calls") and message.tool_calls:
+
+            for tool_call in message.tool_calls:
+
+                print("\nTool called:")
+                print(f"Name: {tool_call['name']}")
+                
+                print("\nArguments:")
+                print(tool_call["args"])
+
+
+    # -----------------------------
+    # Final Answer
+    # -----------------------------
+    print("\n" + "=" * 60)
+    print("FINAL ANSWER")
+    print("=" * 60)
+
+    print(result["messages"][-1].content)
+    
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
